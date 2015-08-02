5e7af0a0e6225cf4ab4467860e7601ea33a5645a
leozdgao <gzdleo0816@gmail.com>
2015-08-02 22:28:21 +0800

    improve ajax

--- common/ajax.js
+++ common/ajax.js
@@ -1,14 +1,15 @@
 var Promise = require('promise-polyfill')
 
 var isDefined = function(val) { return val != null }
-var ajax = function(opts, onLoad, onError) {
+var ajax = asyncRequest = function(opts, fulfill, reject) {
   opts = opts || {}
 
-  var url = opts.url, body = opts.url, headers = opts.url,
+  var url = opts.url, body = opts.body, headers = opts.headers,
     method = opts.method || 'GET', timeout = opts.timeout || 5000
 
   var xhr = new XMLHttpRequest()
   xhr.open(method, url)
+  xhr.withCredentials = !!opts.withCredentials
   for(var key in headers) if(headers.hasOwnProperty(key)) {
       xhr.setRequestHeader(key, headers[key])
   }
@@ -17,7 +18,7 @@ var ajax = function(opts, onLoad, onError) {
       xhr.timeout = timeout
       xhr.ontimeout = function() {
         xhr.hasTimeout = true
-        onError.call(null, xhr)
+        reject({timeout: true})
       }
     }
     else {
@@ -29,15 +30,20 @@ var ajax = function(opts, onLoad, onError) {
   }
   xhr.onreadystatechange = function() {
     if(xhr.readyState == 4) {
-      if(/^(?:2\d{2}|304)$/.test(xhr.status)) {
-        let res = xhr.response && JSON.parse(xhr.response)
-        onLoad.call(null, res, xhr)
+      var res
+      try {
+        res = xhr.response && JSON.parse(xhr.response)
       }
-      else onError.call(null, xhr)
+      catch(e) { res = xhr.response }
+
+      if(/^(?:2\d{2}|304)$/.test(xhr.status)) {
+        fulfill(res)
+      }
+      else reject(res)
     }
   }
   xhr.onabort = function() {
-    onError.call(null, xhr)
+    reject({abort: true})
   }
 
   if(FormData && body instanceof FormData) xhr.send(body)
@@ -50,48 +56,8 @@ var ajax = function(opts, onLoad, onError) {
 }
 
 ajax.promise = function(opts) {
-  opts = opts || {}
-
-  var url = opts.url, body = opts.url, headers = opts.url,
-    method = opts.method || 'GET', timeout = opts.timeout || 5000
-
   return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest()
-    xhr.open(method, url)
-    for(var key in headers) if(headers.hasOwnProperty(key)) {
-        xhr.setRequestHeader(key, headers[key])
-    }
-    if(timeout > 0) {
-      if(isDefined(xhr.timeout)) {
-        xhr.timeout = timeout
-        xhr.ontimeout = function() {
-          xhr.hasTimeout = true
-          reject(xhr)
-        }
-      }
-      else {
-        setTimeout(function() {
-          xhr.abort()
-          xhr.hasTimeout = true
-          reject(xhr)
-        }, timeout)
-      }
-    }
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState == 4) {
-        if(/^(?:2\d{2}|304)$/.test(xhr.status)) {
-          let res = xhr.response && JSON.parse(xhr.response)
-          resolve(res)
-        }
-        else reject(xhr)
-      }
-    }
-
-    if(FormData && body instanceof FormData) xhr.send(body)
-    else {
-      xhr.setRequestHeader("Content-Type", "application/json")
-      xhr.send(JSON.stringify(body))
-    }
+    asyncRequest(opts, resolve, reject)
   })
 }
 
